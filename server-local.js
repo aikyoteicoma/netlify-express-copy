21ed93d63f04f3202b33f54c3127b9b3ec6bc5f9
naoki tsukamura <38200302+aikyoteicoma@users.noreply.github.com>
2020-05-04 08:53:12 +0900

    test

--- server-local.js
+++ server-local.js
@@ -1,5 +1,28 @@
 'use strict';
 
+var http =require('http');
+var express=require('express');
+var morgan =require('morgan');
+var fs=require('fs');
+
+var app =express();
+
+var data=fs.readFileSync("./express/index.html", "utf-8");
+
+
+app.use(morgan('combined'));
+
+app.get("/",function(req,res){
+
+    return res.send(data);
+});
+
+
+var server = http.createServer(app);
+server.listen(3000);
+
+
+/*
 const app = require('./express/server');
 
-app.listen(3000, () => console.log('Local app listening on port 3000!'));
+app.listen(3000, () => console.log('Local app listening on port 3000!'));*/
